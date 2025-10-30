45d9f3311ffde381f0c23469653653cf9ed1bf64
student-a502 <student-a502@STUD-RM19-A502>
2025-10-30 18:29:32 +0500

    Создание класса Author. Коневский

---
+++ Pr12_Book/Classes/Author.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr12_Book.Classes
+{
+    internal class Author
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public Author(int Id, string FIO)
+        {
+            this.Id = Id;
+            this.FIO = FIO;
+        }
+        public static List<Author> AllAuthor()
+        {
+            List<Author> allAuthors = new List<Author>();
+            allAuthors.Add(new Author(1, "Виктор Пелевин"));
+            allAuthors.Add(new Author(2, "Александра Маринина"));
+            allAuthors.Add(new Author(2, "Ольга Герр"))ж
+            return allAuthors;
+        }
+    }
+}
